483db193d929ddd6fbc1a78c1297a21aa049bf7a
Testimony Fresh <testimonyfresh49@gmail.com>
2025-08-10 16:05:03 +0000

    Caj you function the app download now?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,14..72,200..900;1,14..72,200..900&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#333333" />
       </head>
       <body className="font-body antialiased">
         {children}
